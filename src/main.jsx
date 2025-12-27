7df35ba40060175b72a54fce2a0226a2fa3b13f3
ferdiansyah <ferdiansyah@192.168.1.7>
2025-12-27 16:40:22 +0700

    terbeu

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Tambahkan ini
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Ganti 'nama-repo-anda' dengan nama repository GitHub Anda */}
+    <BrowserRouter basename="/portofolio">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
